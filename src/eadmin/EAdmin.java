3df1fe9ff59ed1fd1797441f654170d5d21d1fe7
alexojeac <ojea.corregidor.alejandro.ald@gmail.com>
2024-11-21 19:28:38 +0100

    Build proyect

--- src/eadmin/EAdmin.java
+++ src/eadmin/EAdmin.java
@@ -1,4 +1,5 @@
 package eadmin;
+import utils.Constants;
 
 /**
  *
@@ -6,14 +7,21 @@ package eadmin;
  */
 import controller.LoginController;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 import views.LoginView;
 
 public class EAdmin {
 
     public static void main(String[] args) throws SQLException, Exception {
-        LoginView view = new LoginView();
-        LoginController controller = new LoginController(view);
-        view.setVisible(true);
+        try {
+            LoginView view = new LoginView();
+            LoginController controller = new LoginController(view);
+            view.setVisible(true);
+        } catch (RuntimeException e) {
+            JOptionPane.showMessageDialog(null, Constants.ACCES_ERROR, Constants.ERROR, JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+
     }
-    
+
 }
